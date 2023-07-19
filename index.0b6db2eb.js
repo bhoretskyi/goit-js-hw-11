65c3cf373d3de187482bd96de0b729f6023c1458
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-07-19 19:25:13 +0000

    Deploying to gh-pages from @ bhoretskyi/goit-js-hw-11@7fac6660242e5d3173ceeecc7821b6fd6f37d95a 🚀

--- index.0b6db2eb.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world"),fetch("https://jsonplaceholder.typicode.com/posts").then((function(o){return o.json()})).then((function(o){return console.log(o)}));
-//# sourceMappingURL=index.0b6db2eb.js.map
