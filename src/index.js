7fac6660242e5d3173ceeecc7821b6fd6f37d95a
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-07-19 21:24:53 +0200

    added_axios/nottifix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-console.log('Hello world');
-fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => console.log(json))
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.info('Cogito ergo sum');
+axios
+  .get('https://pixabay.com/api/?key=38354710-a3d6b3af700cce2a78ac34292')
+  .then(response => response.data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
