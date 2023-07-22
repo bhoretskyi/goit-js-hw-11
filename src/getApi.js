a601bf2d85c24df0cc2cb8d25f2e8feb0dfe718a
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-07-22 22:14:31 +0200

    send_homework

--- src/getApi.js
+++ src/getApi.js
@@ -1,24 +1,20 @@
 import axios, { AxiosHeaders } from 'axios';
-AxiosHeaders.page = 1
+import Notiflix, { Notify } from 'notiflix';
+AxiosHeaders.page = 1;
 // axios.defaults.baseURL =
 //   'https://pixabay.com/api/?key=38354710-a3d6b3af700cce2a78ac34292';
-export async function getImages(value,page = 1) {
-  
+export async function getImages(value, page = 1) {
   const images = await axios.get(`https://pixabay.com/api/`, {
     params: {
       key: '38354710-a3d6b3af700cce2a78ac34292',
-      q:`${value}`,
-      image_type: "photo",
-      orientation: "horizontal",
+      q: `${value}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
       safesearch: true,
-      per_page: 3,
-      page: page
+      per_page: 40,
+      page: page,
     },
-   
   });
-  console.log(images);
-
- 
-
-  return images
+  
+return images
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Notiflix from 'notiflix';
+import Notiflix, { Notify } from 'notiflix';
 import { getImages } from './getApi';
 import axios, { AxiosHeaders } from 'axios';
 
@@ -11,26 +11,29 @@ const refs = {
 };
 refs.loader.hidden = true;
 refs.button.disabled = true;
-refs.input.addEventListener('input', ()=>{
-  refs.button.disabled = false
-})
-
-
-
+refs.input.addEventListener('input', () => {
+  refs.button.disabled = false;
+});
 
 refs.button.addEventListener('click', e => {
   e.preventDefault();
 
   newImages(refs.input.value);
- 
 });
+let hits = 0;
 let page = 1;
 async function newImages(value) {
   const desktop = await getImages(value, page)
     .then(resp => {
       if (resp.status !== 200) {
         throw new Error('error');
-      }  
+      }
+     
+      if(resp) {
+        Notify.success(`Hooray! We found ${resp.data.totalHits} images.`)
+      }
+      hits += resp.data.hits.length;
+
       page += 1;
       refs.loader.hidden = false;
       refs.gallery.innerHTML = '';
@@ -67,14 +70,23 @@ async function newImages(value) {
 }
 async function addImages(value) {
   refs.loader.hidden = true;
+
   const desktop = await getImages(value, page)
     .then(resp => {
+      hits += resp.data.hits.length;
       page += 1;
-      console.log(page);
+
       if (resp.status !== 200) {
         throw new Error('error');
       }
+      if (hits > resp.data.totalHits) {
+        refs.loader.hidden = true
+        Notiflix.Report.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
       refs.loader.hidden = false;
+      console.log(hits);
       return resp.data.hits;
     })
     .catch(err =>
